678ec70421cf0c32f6c844a20d431900e1043a3f
rajeshzana1206-glitch <rajeshzana1206@gmail.com>
2026-09-12 23:43:46 +0530

    Add files via upload

---
+++ student profile manager.py
@@ -0,0 +1,17 @@
+student = {
+    "name": "Rajesh",
+    "course": "Python",
+    "mark": 85
+}
+
+print("name:", student["name"])
+print("course:", student["course"])
+
+student["mark"] = 95
+student["result"] = "pass"
+print("Final student record:")
+print(student)
+
+
+
+
